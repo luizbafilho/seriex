3f5f19877dcc9c8ac5a6b60ef36d07028055d434
Luiz Bezerra <luiz.filho@corp.globo.com>
2015-12-15 22:49:11 -0200

    adding initial views

--- web/controllers/series_controller.ex
+++ web/controllers/series_controller.ex
@@ -1,11 +1,18 @@
 defmodule Seriex.SeriesController do
   use Seriex.Web, :controller
 
+  def index(conn, %{"q" => query}) do
+    shows = TVMaze.search(query)
+    render conn, "index.html", shows: shows
+  end
+
   def index(conn, _params) do
-    render conn, "index.html"
+    render conn, "index.html", shows: []
   end
 
   def show(conn, %{"id" => id}) do
-    render conn, "show.html", id: id
+    serie = TVMaze.fetch(id)
+
+    render conn, "show.html", serie: serie
   end
 end
--- web/views/series_view.ex
+++ web/views/series_view.ex
@@ -1,3 +1,7 @@
 defmodule Seriex.SeriesView do
   use Seriex.Web, :view
+
+  def seasons(%{"_embedded" => %{"episodes" => episodes}}) do
+    episodes |> Enum.chunk_by(&(&1["season"]))
+  end
 end
